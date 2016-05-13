4a75732271a11e6052da9cea341d120180cce446
SYFH <syfh@live.com>
2016-05-13 17:23:55 +0800

    解决在xib和SB上使用时不显示的问题

--- FBCScoreStar/FBCScoreStar.m
+++ FBCScoreStar/FBCScoreStar.m
@@ -20,51 +20,59 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        
-        //使用时, 视图的宽度必须大于高度
-        NSAssert(!(frame.size.width < frame.size.height), @"视图的宽度小于高度!");
-        
-        self.backgroundColor = [UIColor colorWithWhite:0.85 alpha:1];
-        NSInteger num = frame.size.width / frame.size.height;//个数
-        
-        //进度
-        UIBezierPath *backgroudPath = [UIBezierPath bezierPath];
-        [backgroudPath moveToPoint:CGPointMake(0, frame.size.height * 0.5)];
-        [backgroudPath addLineToPoint:CGPointMake(frame.size.width, frame.size.height * 0.5)];
-        CAShapeLayer *backgroudLayer = [CAShapeLayer layer];
-        backgroudLayer.path = backgroudPath.CGPath;
-        backgroudLayer.lineWidth = frame.size.height;
-        backgroudLayer.strokeColor = [UIColor orangeColor].CGColor;
-        backgroudLayer.fillColor = [UIColor orangeColor].CGColor;
-        [self.layer addSublayer:backgroudLayer];
-        self.backgroudLayer = backgroudLayer;
-        
-        //画图
-        UIBezierPath *star = [[UIBezierPath alloc] init];
-        for (int i = 1; i < num * 2; i ++) {
-            if (i % 2 != 0) {
-                CGFloat startX = frame.size.width / (frame.size.width / frame.size.height) * i;
-                CGPoint center = CGPointMake(startX * 0.5, frame.size.height * 0.5);
-                CGFloat radius = frame.size.height * 0.5;
-                CGFloat angle = 4 * M_PI / 5;
-                
-                //画五角星
-                [star moveToPoint:CGPointMake(startX * 0.5, 0)];
-                for (int i = 0; i < 5; i ++) {
-                    CGFloat x = center.x - sinf((i + 1) * angle) * radius;
-                    CGFloat y = center.y - cosf((i + 1) * angle) * radius;
-                    [star addLineToPoint:CGPointMake(x, y)];
-                }
-                [star addLineToPoint:CGPointMake(startX * 0.5, 0)];
-            }
-        }
-        
-        //遮罩
-        CAShapeLayer *starLayer = [CAShapeLayer layer];
-        starLayer.path = star.CGPath;
-        self.layer.mask = starLayer;
+        [self drawStarWithRect:frame];
     }
     return self;
+}
+
+- (void)awakeFromNib {
+    [self drawStarWithRect:self.frame];
+}
+
+- (void)drawStarWithRect:(CGRect)frame {
+    
+    //使用时, 视图的宽度必须大于高度
+    NSAssert(!(frame.size.width < frame.size.height), @"视图的宽度小于高度!");
+    
+    self.backgroundColor = [UIColor colorWithWhite:0.85 alpha:1];
+    NSInteger num = frame.size.width / frame.size.height;//个数
+    
+    //进度
+    UIBezierPath *backgroudPath = [UIBezierPath bezierPath];
+    [backgroudPath moveToPoint:CGPointMake(0, frame.size.height * 0.5)];
+    [backgroudPath addLineToPoint:CGPointMake(frame.size.width, frame.size.height * 0.5)];
+    CAShapeLayer *backgroudLayer = [CAShapeLayer layer];
+    backgroudLayer.path = backgroudPath.CGPath;
+    backgroudLayer.lineWidth = frame.size.height;
+    backgroudLayer.strokeColor = [UIColor orangeColor].CGColor;
+    backgroudLayer.fillColor = [UIColor orangeColor].CGColor;
+    [self.layer addSublayer:backgroudLayer];
+    self.backgroudLayer = backgroudLayer;
+    
+    //画图
+    UIBezierPath *star = [[UIBezierPath alloc] init];
+    for (int i = 1; i < num * 2; i ++) {
+        if (i % 2 != 0) {
+            CGFloat startX = frame.size.width / (frame.size.width / frame.size.height) * i;
+            CGPoint center = CGPointMake(startX * 0.5, frame.size.height * 0.5);
+            CGFloat radius = frame.size.height * 0.5;
+            CGFloat angle = 4 * M_PI / 5;
+            
+            //画五角星
+            [star moveToPoint:CGPointMake(startX * 0.5, 0)];
+            for (int i = 0; i < 5; i ++) {
+                CGFloat x = center.x - sinf((i + 1) * angle) * radius;
+                CGFloat y = center.y - cosf((i + 1) * angle) * radius;
+                [star addLineToPoint:CGPointMake(x, y)];
+            }
+            [star addLineToPoint:CGPointMake(startX * 0.5, 0)];
+        }
+    }
+    
+    //遮罩
+    CAShapeLayer *starLayer = [CAShapeLayer layer];
+    starLayer.path = star.CGPath;
+    self.layer.mask = starLayer;
 }
 
 - (void)setStarBGColor:(UIColor *)starBGColor {
